20755c5c6196336e6cb982600e37436b81f3e05b
Shheezy <simon.roland15@gmail.com>
2022-07-14 18:12:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const tmi = require('tmi.js');
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000 ;
 
 app.get('/', (req, res) => res.send('Yo boi!!'))
 
