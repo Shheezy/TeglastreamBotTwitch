597fe08be97b6844e29ab5b2f42c25e0f6460d79
Shheezy <simon.roland15@gmail.com>
2022-11-30 13:51:37 +0100

    update.1.1

--- app.js
+++ app.js
@@ -106,6 +106,10 @@ function onChatHandler(target, context, msg, self) {
     client.say(target, "Woof Woof! Jön Hoffmann letépi a kezed ha megint ilyen hülyeséget kérdezel! DansGame");
   }
 
+  if (commandName === '!health') {
+    console.log(response.status);
+  }
+
   if (commandName === '!color') {
     //console.log(client.getChannels());
     client.color(colors[Math.floor(Math.random() * 10)]);
