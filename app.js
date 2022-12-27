41db2d3e8b1d302b7ceb6cb2c5c84ed169920b3f
Shheezy <simon.roland15@gmail.com>
2022-12-27 08:02:26 +0100

    Update

--- app.js
+++ app.js
@@ -44,24 +44,16 @@ function onChatHandler(target, context, msg, self) {
 
   const commandName = msg.trim();
 
+  if (commandName === '!parancsok') {
+    client.say(target, 'A chatben használható parancsok: !donate, !sub, !fb, !rp, !interju, !taka, !tiktok, !yt, !rules, !dc');
+  }
+
   if (commandName === '!dc') {
-    client.say(target, "Csatlakozz a Discord szerveremre CZimbi! https://discord.teglastream.hu/");
-  }
-
-  if (commandName === '!cup') {
-    client.say(target, "Versenyeznél a spanoddal Rocket Leagueben? Akkor gyere el az első Tiszanekeresd Amateur Cup versenyére, December 25.-én ahol 3800 Credit kerül szétosztásra a döntősök között! További információk és jelentkezés: https://cup.teglastream.hu");
-  }
-
-  if (commandName === '!12') {
-    client.say(target, "A jótékonysági stream December 18.-án vasárnap reggel 10 órától, este 10 óráig lesz megtartva!");
+    client.say(target, "Tiszanekeresd Discord szervere: https://discord.teglastream.hu/");
   }
 
   if (commandName === '!sub') {
     client.say(target, 'Ha nem működne a sub Nálad czimbi, ezen a linken keresztül próbálkozz! https://www.twitch.tv/subs/teglaofficial');
-  }
-
-  if (commandName === '!subtember') {
-    client.say(target, 'Ha szeptemberben vásárolsz subot 20% kedvezményt kapsz 1 hónapos előfizetés esetén. Ha 3 hónapra fizetsz elő 25%-ot takaríthatsz meg, illetve ha 6 hónapra fizetsz elő, 30%-al kevesebbet fizetsz majd!');
   }
 
   if (commandName === '!rules') {
@@ -73,7 +65,7 @@ function onChatHandler(target, context, msg, self) {
   }
 
   if (commandName === '!fb') {
-    client.say(target, 'Megköszönöm czimbim ha Facebook-on is bekövetsz! https://www.facebook.com/Teglaofficial');
+    client.say(target, 'Facebook: https://www.facebook.com/Teglaofficial');
   }
 
   if (commandName === '!interju') {
@@ -85,23 +77,15 @@ function onChatHandler(target, context, msg, self) {
   }
 
   if (commandName === '!4mahun') {
-    client.say(target, 'A 4MA Roleplay Discord szervere: https://discord.gg/nstnTQwUD4');
+    client.say(target, 'A 4MA Hungary Discord szervere: https://discord.gg/nstnTQwUD4');
   }
 
   if (commandName === '!tiktok') {
-    client.say(target, 'Elérhető vagyok TikTok-on is Czimbi. Dobom is a linket. https://www.tiktok.com/@tegla_official');
+    client.say(target, 'TikTok: https://www.tiktok.com/@tegla_official');
   }
 
   if (commandName === '!yt') {
-    client.say(target, 'Ez a YouTube csatim. Megköszönöm ha feliratkozol ott is. https://www.youtube.com/TeglaOfficial');
-  }
-
-  if (commandName === '!taka') {
-    client.say(target, 'Nem bírsz viselkedni?!!?Jön Jusztika és el takarít a lépcsőházból!!');
-  }
-
-  if (commandName === '!parancsok') {
-    client.say(target, 'A chatben használható parancsok: !donate, !sub, !fb, !rp, !interju, !taka, !tiktok, !yt, !rules, !dc');
+    client.say(target, 'Youtube: https://www.youtube.com/TeglaOfficial');
   }
 
   if (msg.includes("rp?") || msg.includes("RP?") || msg.includes("Rp?")) {
@@ -116,19 +100,14 @@ function onChatHandler(target, context, msg, self) {
 }
 
 function DiscTimer() {
-  client.action('teglaofficial', 'Üzleti dolgokkal kapcsolatban írj egy e-mailt a teglastream@gmail.com címre!');
+  client.action('teglaofficial', 'Csatllakozz Te is Tiszanekeresdhez: https://discord.teglastream.hu');
 }
 setInterval(DiscTimer, 1500000); //25min
 
 function BussinessTimer() {
-  client.action('teglaofficial', 'Csatlakozz Discord szerveremre Czimbi: https://discord.teglastream.hu');
+  client.action('teglaofficial', 'Üzleti dolgokkal kapcsolatban írj egy e-mailt a teglastream@gmail.com címre!');
 }
 setInterval(BussinessTimer, 2500000); //41min
-
-function YoutubeTimer() {
-  client.action('teglaofficial', 'A VOD-okat mostantól itt éred el czimbi: https://www.youtube.com/TeglaOfficial');
-}
-setInterval(YoutubeTimer, 2000000); //41min
 
 function checkChat(channel, username, message) {
   console.log(username.username, message)
